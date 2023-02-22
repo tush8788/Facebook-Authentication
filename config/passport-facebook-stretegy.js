ba952e87b2629fd92930030b7b837502d0d9cdbc
tush87 <tusharj8788@gmail.com>
2023-02-23 00:47:15 +0530

    add .env

--- config/passport-facebook-stretegy.js
+++ config/passport-facebook-stretegy.js
@@ -1,11 +1,12 @@
 const passport=require('passport');
 const facebookStategy=require('passport-facebook').Strategy;
 const UserDB=require('../models/user');
+const dotenv=require('dotenv').config();
 
 passport.use(new facebookStategy({
-    clientID:1141094419937314,
-    clientSecret:"247a322e25d3e6d143cb7fc2d7467dbf",
-    callbackURL:"http://localhost:8000/user/auth/facebook",
+    clientID:process.env.clientID,
+    clientSecret:process.env.clientSecret,
+    callbackURL:process.env.callbackURL,
     profileFields:['email','name','displayName','photos']
 },async function(accessToken, refreshToken, profile, done){
     // console.log(profile);
